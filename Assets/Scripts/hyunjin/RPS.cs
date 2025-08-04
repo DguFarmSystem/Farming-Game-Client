e1157177c63bfad22c14a8bc5786db8597724999
hyunjin <hyunjin6918@naver.com>
2025-08-04 21:27:11 +0900

    Reapply "FIX: 미니게임 아트 추가"
    This reverts commit 7b35ebd8483754904bfaf15449a145f03e3a894d.

--- Assets/Scripts/hyunjin/RPS.cs
+++ Assets/Scripts/hyunjin/RPS.cs
@@ -10,37 +10,42 @@ public class RPS : MonoBehaviour
     public Image winBox, drawBox, loseBox;
     public Button rockButton, paperButton, scissorsButton;
     public Button desciptionButton, startButton;
-    public TMP_Text winCountText;
+    public Image winCountImg;
+    public Sprite win0, win1, win2, win3;
 
     public GameResult gameResult;
 
-    private Sprite[] sprites;
+    private Sprite[] choiceSprites;
+    private Sprite[] winCountSprites;
     public int round = 0;
     public int winCount = 0;
     public int current = 0;
     public bool isShuffling = false;
 
+    private Coroutine autoExitCoroutine;
+
     void Start()
     {
-        sprites = new Sprite[] { rockSprite, paperSprite, scissorsSprite };
+        choiceSprites = new Sprite[] { rockSprite, paperSprite, scissorsSprite };
+        winCountSprites = new Sprite[] { win0, win1, win2, win3 };
         startButton.onClick.AddListener(StartGame);
         rockButton.onClick.AddListener(() => PlayerSelect(0));
         paperButton.onClick.AddListener(() => PlayerSelect(1));
         scissorsButton.onClick.AddListener(() => PlayerSelect(2));
-
-        winCountText.text = "";
+        winBox.gameObject.SetActive(false);
+        loseBox.gameObject.SetActive(false);
+        drawBox.gameObject.SetActive(false);
+        winCountImg.sprite = winCountSprites[0];
     }
 
     void StartGame()
     {
+        startButton.enabled = false;
         round = 0;
         winCount = 0;
-        winCountText.text = $"win count : {winCount}/3";
-        winBox.gameObject.SetActive(false);
-        loseBox.gameObject.SetActive(false);
-        drawBox.gameObject.SetActive(false);
+        winCountImg.sprite = winCountSprites[winCount];
+        
         // 게임 전체 횟수 ++
-
         StartCoroutine(ShuffleImages());
     }
 
@@ -50,17 +55,22 @@ public class RPS : MonoBehaviour
         winBox.gameObject.SetActive(false);
         loseBox.gameObject.SetActive(false);
         drawBox.gameObject.SetActive(false);
+
+        autoExitCoroutine = StartCoroutine(AutoExitCoroutine());
         while (isShuffling) {
-            currentImage.sprite = sprites[current];
+            currentImage.sprite = choiceSprites[current];
             current = (current + 1) % 3;
             yield return new WaitForSeconds(0.1f);
-            // 120초간 버튼 미클릭 시 entrance로
         }
     }
 
     void PlayerSelect(int player)
     {
         if (!isShuffling) return;
+        if (autoExitCoroutine != null) {
+            StopCoroutine(autoExitCoroutine);
+            autoExitCoroutine = null;
+        }
 
         isShuffling = false;
         round++;
@@ -68,31 +78,33 @@ public class RPS : MonoBehaviour
 
         if (player == com) {
             drawBox.gameObject.SetActive(true);
-            Debug.Log($"com: {com} / you: {player} => draw");
         }
         else if ((player == 0 && com == 2) || (player == 1 && com == 0) || (player == 2 && com == 1)) {
             winBox.gameObject.SetActive(true);
             winCount++;
-            winCountText.text = $"win count : {winCount}/3";
-            Debug.Log($"com: {com} / you: {player} => win");
+            winCountImg.sprite = winCountSprites[winCount];
         }
         else {
             loseBox.gameObject.SetActive(true);
-            Debug.Log($"com: {com} / you: {player} => lose");
         }
         StartCoroutine(Hold());
     }
 
     IEnumerator Hold()
     {
-        Debug.Log("hold 3f");
         yield return new WaitForSeconds(3f);
-        Debug.Log("hold end");
 
         if (round == 3)
             gameResult.SaveResult("RPS", winCount);
         else
             StartCoroutine(ShuffleImages());
+    }
+
+    IEnumerator AutoExitCoroutine()
+    {
+        yield return new WaitForSeconds(120f);
+        Debug.Log("미입력 120초 경과, 미니게임 종료");
+        SceneLoader.Instance.ExitMiniGame();
     }
 
 }
